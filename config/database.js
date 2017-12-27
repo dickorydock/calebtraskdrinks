750aca5503496f0864f5c49cc4dc93a14b0c26cd
Dickory Dock <nickcommins@gmail.com>
2017-12-26 21:43:49 -0500

    change mongo

--- config/database.js
+++ config/database.js
@@ -1,4 +1,4 @@
 // config/database.js
 module.exports = {
- 'url' : 'mongodb://erik2:1Knowthingsnow@ds139242.mlab.com:39242/heroku_xm8cw9z4'
+ 'url' : 'mongodb://erik2:1Knowthingsnow@ds163796.mlab.com:63796/heroku_971ftwft'
 }
