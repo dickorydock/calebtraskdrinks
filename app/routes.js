ebc7e8cb8619ec53fc2a34003871d2daef469864
Dickory Dock <nickcommins@gmail.com>
2017-12-28 21:19:36 -0500

    adding twitter

--- app/routes.js
+++ app/routes.js
@@ -25,6 +25,21 @@ module.exports = function(app, passport) {
         failureFlash : true // allow flash messages
     }));
     
+
+    // =====================================
+    // TWITTER ROUTES ======================
+    // =====================================
+    // route for twitter authentication and login
+    app.get('/auth/twitter', passport.authenticate('twitter'));
+
+    // handle the callback after twitter has authenticated the user
+    app.get('/auth/twitter/callback',
+        passport.authenticate('twitter', {
+            successRedirect : '/profile',
+            failureRedirect : '/'
+        }));
+
+    
     // =====================================
     // SIGNUP ==============================
     // =====================================
@@ -48,7 +63,7 @@ module.exports = function(app, passport) {
     // we will use route middleware to verify this (the isLoggedIn function)
     app.get('/profile', isLoggedIn, function(req, res) {
         res.render('pages/profile.ejs', {
-            
+
             user : req.user // get the user out of session and pass to template
         });
     });
