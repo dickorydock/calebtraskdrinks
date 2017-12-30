5292526cc5ede461557a650f3bc3a98cae7875a7
Dickory Dock <nickcommins@gmail.com>
2017-12-30 00:30:19 -0500

    boy ohdaf

--- app/routes.js
+++ app/routes.js
@@ -91,7 +91,8 @@ module.exports = function(app, passport, survey) {
         var incVar = 'surveyResponses.'+req.body.text;
         console.log(req.params.id);
         console.log(incVar);
-        userSurveys.update({'_id':  req.params.id}, {"$inc": {surveyActive: 1}});
+        // userSurveys.update({'_id':  req.params.id}, {"$inc": {surveyActive: 1}});
+        userSurveys.update({'_id':  req.params.id}, {surveyActive:9});
         res.redirect('/profile');
         
         // res.render('pages/survey.ejs', {
