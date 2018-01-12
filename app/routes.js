83fe860953ae48ea42a303acaf9a25ce56760a65
Dickory Dock <nickcommins@gmail.com>
2018-01-12 13:51:22 -0500

    back in the days

--- app/routes.js
+++ app/routes.js
@@ -21,66 +21,66 @@ module.exports = function(app, passport, survey) {
         res.render('pages/login.ejs', { message: req.flash('loinMessage') }); 
     });
 
-     function saveInstance() {
+  //    function saveInstance() {
     
-      return businessVisitors.findOne(
-        {
-            "$and": [
-                {
-                   "userId": userId
-                }
-                ,{
-                    "yelpId": yelpId
-                }
-            ]
-        }
-      )
-      .exec()
-      .then(data => {
-       if (!data) {
-          console.log("adding "+yelpId+" with "+userId);
-          var localISOTime  = (new Date(Date.now() - tzOffset)).toISOString().slice(0,-1)  
-          var newVisitor = new businessVisitors();
-          newVisitor.userId             = userId;
-          newVisitor.yelpId             = yelpId;
-          newVisitor.isGoingToday       = true;
-          newVisitor.lastResponseDate   = localISOTime;
-          return instancerecord.save();
-        }
-        return data;
-      })
-  };
+  //     return businessVisitors.findOne(
+  //       {
+  //           "$and": [
+  //               {
+  //                  "userId": userId
+  //               }
+  //               ,{
+  //                   "yelpId": yelpId
+  //               }
+  //           ]
+  //       }
+  //     )
+  //     .exec()
+  //     .then(data => {
+  //      if (!data) {
+  //         console.log("adding "+yelpId+" with "+userId);
+  //         var localISOTime  = (new Date(Date.now() - tzOffset)).toISOString().slice(0,-1)  
+  //         var newVisitor = new businessVisitors();
+  //         newVisitor.userId             = userId;
+  //         newVisitor.yelpId             = yelpId;
+  //         newVisitor.isGoingToday       = true;
+  //         newVisitor.lastResponseDate   = localISOTime;
+  //         return instancerecord.save();
+  //       }
+  //       return data;
+  //     })
+  // };
 
-    app.post('/gotoBar', function(req,res){
-        var businessVisitors = require('./models/businessVisitor');
-        var users = require('./models/user');
-        return businessVisitors.findOne(
-        {
-            "$and": [
-                {
-                   "userId": userId
-                }
-                ,{
-                    "yelpId": yelpId
-                }
-            ]
-        }
-      )
-      .exec()
-      .then(data => {
-       if (!data) {
-          console.log("adding "+yelpId+" with "+userId);
-          var localISOTime  = (new Date(Date.now() - tzOffset)).toISOString().slice(0,-1)  
-          var newVisitor = new businessVisitors();
-          newVisitor.userId             = userId;
-          newVisitor.yelpId             = yelpId;
-          newVisitor.isGoingToday       = true;
-          newVisitor.lastResponseDate   = localISOTime;
-          return instancerecord.save();
-        }
-        return data;
-      })
-    })
+    // app.post('/gotoBar', function(req,res){
+    //     var businessVisitors = require('./models/businessVisitor');
+    //     var users = require('./models/user');
+    //     return businessVisitors.findOne(
+    //     {
+    //         "$and": [
+    //             {
+    //                "userId": userId
+    //             }
+    //             ,{
+    //                 "yelpId": yelpId
+    //             }
+    //         ]
+    //     }
+    //   )
+    //   .exec()
+    //   .then(data => {
+    //    if (!data) {
+    //       console.log("adding "+yelpId+" with "+userId);
+    //       var localISOTime  = (new Date(Date.now() - tzOffset)).toISOString().slice(0,-1)  
+    //       var newVisitor = new businessVisitors();
+    //       newVisitor.userId             = userId;
+    //       newVisitor.yelpId             = yelpId;
+    //       newVisitor.isGoingToday       = true;
+    //       newVisitor.lastResponseDate   = localISOTime;
+    //       return instancerecord.save();
+    //     }
+    //     return data;
+    //   })
+    // })
 
    // process the login form
     app.post('/login', passport.authenticate('local-login', {
--- public/js/scripts.js
+++ public/js/scripts.js
@@ -8,20 +8,20 @@ function gotoBar(el){
   var yelpid = el.getAttribute('data-yelpid');
   var mode = el.getAttribute('data-mode'); 
 
-  $.ajax({
-    url:'./gotoBar',
-    cache: false,
-    type: "POST",
-    data:{id:id, yelpid:yelpid, mode:mode},
-  });       
+  // $.ajax({
+  //   url:'./gotoBar',
+  //   cache: false,
+  //   type: "POST",
+  //   data:{id:id, yelpid:yelpid, mode:mode},
+  // });       
 
-  var watchword = "Unwatch"; 
+  // var watchword = "Unwatch"; 
 
-  if (mode=="onx")
-  { 
-    var watchword = "Watch"; 
-  } 
-  var allgenre = ["othe", "broa", "caba", "chil", "conc", "lect", "live", "musi", "play", "spec", "spor", "danc"] ; 
+  // if (mode=="onx")
+  // { 
+  //   var watchword = "Watch"; 
+  // } 
+  // var allgenre = ["othe", "broa", "caba", "chil", "conc", "lect", "live", "musi", "play", "spec", "spor", "danc"] ; 
 
   
   console.log("callback fired genre!!");
