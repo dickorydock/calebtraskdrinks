97935d91b7ee34d035af850725b1ab4a8d4f3a24
Dickory Dock <nickcommins@gmail.com>
2018-01-12 13:22:38 -0500

    ground

---
+++ app/models/businessVisitor.js
@@ -0,0 +1,15 @@
+// app/models/userSurvey.js
+// load the things we need
+var mongoose = require('mongoose');
+mongoose.Promise = global.Promise;
+
+// define the schema for our user model
+var businessVisitorSchema = mongoose.Schema({
+        userId          : String,
+        yelpId			: String,
+        isGoingToday    : Boolean,
+        lastResponseDate:Date
+});
+
+// create the model for users and expose it to our app
+module.exports = mongoose.model('businessVisitor', businessVisitorSchema);
---
+++ public/js/scripts.js
@@ -0,0 +1,32 @@
+        
+
+
+
+function gotoBar(el){
+
+  var id = el.getAttribute('data-userid');
+  var yelpid = el.getAttribute('data-yelpid');
+  var mode = el.getAttribute('data-mode'); 
+
+  $.ajax({
+    url:'./gotoBar',
+    cache: false,
+    type: "POST",
+    data:{id:id, yelpid:yelpid, mode:mode},
+  });       
+
+  var watchword = "Unwatch"; 
+
+  if (mode=="onx")
+  { 
+    var watchword = "Watch"; 
+  } 
+  var allgenre = ["othe", "broa", "caba", "chil", "conc", "lect", "live", "musi", "play", "spec", "spor", "danc"] ; 
+
+  
+  console.log("callback fired genre!!");
+}
+     
+  /*.fail(function(jqXHR, textStatus, errorThrown) {
+  console.log('There has been an error.');
+  });*/
