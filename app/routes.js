b3a8fbbe0d9dd570eddf9982914902e929c49d28
Dickory Dock <nickcommins@gmail.com>
2018-01-13 00:19:02 -0500

    fine weather

--- app/routes.js
+++ app/routes.js
@@ -37,7 +37,7 @@ module.exports = function(app, passport, survey) {
         var users = require('./models/user');
         
             return businessVisitors.update(
-            {"$and": [{"userId": req.body.id},{"yelpId": req.body.yelpid}]}, conditions)
+            {"$and": [{"userId": req.body.id},{"yelpId": req.body.yelpid}]}, myupdate)
             .exec()
             .then(data => {
             if (true) {
